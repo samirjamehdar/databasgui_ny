628a1bedcbbbff5b9a0132c3e1b0efb7348627c3
Samir Jamehdar <samirjamehdar@gmail.com>
2023-03-30 14:22:43 +0200

    Omgjord

---
+++ src/main/java/com/example/databasgui_ny/dao/ActorDAO.java
@@ -0,0 +1,7 @@
+package com.example.databasgui_ny.dao;
+
+import com.example.databasgui_ny.entities.Actor;
+
+public class ActorDAO implements DAO<Actor>{
+
+}
--- src/main/java/com/example/databasgui_ny/util/SessionFactorySingleton.java
+++ src/main/java/com/example/databasgui_ny/util/SessionFactorySingleton.java
@@ -1,9 +1,15 @@
 package com.example.databasgui_ny.util;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class SessionFactorySingleton {
     private static SessionFactory sessionFactory;
     private SessionFactorySingleton() {}
-
+    public static SessionFactory getInstance() {
+        if (sessionFactory == null) {
+            sessionFactory = new Configuration().configure().buildSessionFactory();
+        }
+        return sessionFactory;
+    }
 }
