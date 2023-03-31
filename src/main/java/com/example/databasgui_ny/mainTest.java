dc958b0ba256e3c41441aa326f01b2e2029a1de6
MaxEkwing <maxekwing@hotmail.com>
2023-03-31 11:21:05 +0200

    Omgjord

--- src/main/java/com/example/databasgui_ny/mainTest.java
+++ src/main/java/com/example/databasgui_ny/mainTest.java
@@ -6,6 +6,8 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.List;
+
 public class mainTest {
     public static void main(String[] args) {
         SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
@@ -13,8 +15,13 @@ public class mainTest {
         session.beginTransaction();
         Actor actor = session.get(Actor.class, 1);
 
+        List<Actor> actorList = session.createQuery("FROM Actor").list();
+        System.out.println(actorList.size());
+
         System.out.println(actor.getFirst_name());
         session.getTransaction().commit();
         session.close();
+
+
     }
 }
