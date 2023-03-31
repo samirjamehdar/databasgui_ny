00da6429c309c7278b16c7523e2778a04dc99fc4
MaxEkwing <maxekwing@hotmail.com>
2023-03-31 13:33:57 +0200

    Laggt till handlers till tableview

--- src/main/java/com/example/databasgui_ny/mainGUI/MainMenuController.java
+++ src/main/java/com/example/databasgui_ny/mainGUI/MainMenuController.java
@@ -3,6 +3,8 @@ package com.example.databasgui_ny.mainGUI;
 import com.example.databasgui_ny.dao.ActorDAO;
 import com.example.databasgui_ny.entities.Actor;
 import com.example.databasgui_ny.entities.Address;
+import com.example.databasgui_ny.entities.City;
+import com.example.databasgui_ny.entities.Customer;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -44,6 +46,23 @@ public class MainMenuController implements Initializable {
     private TableColumn<Address, Date> addressLastUpdateCol = new TableColumn<>("Last Update");
     private ObservableList<Address> addressObList = FXCollections.observableArrayList();
 
+    private TableColumn<City, Integer> cityIdCol = new TableColumn<>("City ID");
+    private TableColumn<City, String> cityNameCol = new TableColumn<>("City");
+    private TableColumn<City, String> cityCountryIdCol = new TableColumn<>("Country Id");
+    private TableColumn<City, Date> cityLastUpdateCol = new TableColumn<>("Last Update");
+    private ObservableList<City> cityObList = FXCollections.observableArrayList();
+
+    private TableColumn<Customer, Integer> customerIdCol = new TableColumn<>("Customer ID");
+    private TableColumn<Customer, Integer> customerStoreIdCol = new TableColumn<>("Store ID");
+    private TableColumn<Customer, String> customerFirstNameCol = new TableColumn<>("First Name");
+    private TableColumn<Customer, String> customerLastNameCol = new TableColumn<>("Last Name");
+    private TableColumn<Customer, String> customerEmailCol = new TableColumn<>("E-mail");
+    private TableColumn<Customer, Integer> customerAddressIdCol = new TableColumn<>("Address ID");
+    private TableColumn<Customer, String> customerActiveCol = new TableColumn<>("Active");
+    private TableColumn<Customer, Date> customerCreateDateCol = new TableColumn<>("Created");
+    private TableColumn<Customer, Date> customerLastUpdateCol = new TableColumn<>("Last Update");
+    private ObservableList<Customer> customerObList = FXCollections.observableArrayList();
+
     private String selectedTable;
 
     private final ObservableList<String> menuItems = FXCollections.observableArrayList("Actor", "Address", "City", "Customer", "Film", "Film_actor",
@@ -60,11 +79,11 @@ public class MainMenuController implements Initializable {
                     break;
                 case "Address":
                     selectedTable = "Address";
-                    System.out.println("Address is selected, refresh the table to actor values");
+                    handleAddressTable();
                     break;
                 case "City":
                     selectedTable = "City";
-                    System.out.println("City is selected, refresh the table to actor values");
+                    handleCityTable();
                     break;
                 case "Customer":
                     selectedTable = "Customer";
@@ -130,29 +149,81 @@ public class MainMenuController implements Initializable {
         }
     }
 
-//    public void handleAddressTable() {
-//        if (addressObList.size() == 0) {
-//            AddressDAO addressDAO = new AddressDAO();
-//
-//            addressIdCol.setCellValueFactory(new PropertyValueFactory<>("address_id"));
-//            actorFirstNameCol.setCellValueFactory(new PropertyValueFactory<>("first_name"));
-//            actorLastNameCol.setCellValueFactory(new PropertyValueFactory<>("last_name"));
-//            actorLastUpdateCol.setCellValueFactory(new PropertyValueFactory<>("last_update"));
-//            actorLastUpdateCol.setCellValueFactory(new PropertyValueFactory<>("last_update"));
-//            actorLastUpdateCol.setCellValueFactory(new PropertyValueFactory<>("last_update"));
-//            actorLastUpdateCol.setCellValueFactory(new PropertyValueFactory<>("last_update"));
-//            actorLastUpdateCol.setCellValueFactory(new PropertyValueFactory<>("last_update"));
-//
-//            List<Actor> actorList = actorDAO.readAll();
-//            actorObList.addAll(actorList);
-//
-//            tableView.setItems(actorObList);
-//            tableView.getColumns().add(actorIdCol);
-//            tableView.getColumns().add(actorFirstNameCol);
-//            tableView.getColumns().add(actorLastNameCol);
-//            tableView.getColumns().add(actorLastUpdateCol);
-//        }
-//    }
+    public void handleAddressTable() {
+        if (addressObList.size() == 0) {
+            AddressDAO addressDAO = new AddressDAO();
+
+            addressIdCol.setCellValueFactory(new PropertyValueFactory<>("address_id"));
+            address1Col.setCellValueFactory(new PropertyValueFactory<>("address"));
+            address2Col.setCellValueFactory(new PropertyValueFactory<>("address2"));
+            addressDistrictCol.setCellValueFactory(new PropertyValueFactory<>("district"));
+            addressCityIdCol.setCellValueFactory(new PropertyValueFactory<>("city_id"));
+            addressPostalCol.setCellValueFactory(new PropertyValueFactory<>("postal_code"));
+            addressPhoneCol.setCellValueFactory(new PropertyValueFactory<>("phone"));
+            addressPhoneCol.setCellValueFactory(new PropertyValueFactory<>("last_update"));
+
+            List<Address> addressList = addressDAO.readAll();
+            addressObList.addAll(addressList);
+
+            tableView.setItems(addressObList);
+            tableView.getColumns().add(addressIdCol);
+            tableView.getColumns().add(address1Col);
+            tableView.getColumns().add(address2Col);
+            tableView.getColumns().add(addressDistrictCol);
+            tableView.getColumns().add(addressCityIdCol);
+            tableView.getColumns().add(addressPostalCol);
+            tableView.getColumns().add(addressPhoneCol);
+            tableView.getColumns().add(addressLastUpdateCol);
+        }
+    }
+    public void handleCityTable() {
+        if (cityObList.size() == 0) {
+            CityDAO cityDAO = new CityDAO();
+
+            cityIdCol.setCellValueFactory(new PropertyValueFactory<>("actor_id"));
+            cityNameCol.setCellValueFactory(new PropertyValueFactory<>("city"));
+            cityCountryIdCol.setCellValueFactory(new PropertyValueFactory<>("country_id"));
+            cityLastUpdateCol.setCellValueFactory(new PropertyValueFactory<>("last_update"));
+
+            List<City> cityList = cityDAO.readAll();
+            cityObList.addAll(cityList);
+
+            tableView.setItems(cityObList);
+            tableView.getColumns().add(cityIdCol);
+            tableView.getColumns().add(cityNameCol);
+            tableView.getColumns().add(cityCountryIdCol);
+            tableView.getColumns().add(cityLastUpdateCol);
+        }
+    }
+    public void handleAddressTable() {
+        if (customerObList.size() == 0) {
+            CustomerDAO customerDAO = new CustomerDAO();
+
+            customerIdCol.setCellValueFactory(new PropertyValueFactory<>("customer_id"));
+            customerStoreIdCol.setCellValueFactory(new PropertyValueFactory<>("store_id"));
+            customerFirstNameCol.setCellValueFactory(new PropertyValueFactory<>("first_name"));
+            customerLastNameCol.setCellValueFactory(new PropertyValueFactory<>("last_name"));
+            customerEmailCol.setCellValueFactory(new PropertyValueFactory<>("email"));
+            customerAddressIdCol.setCellValueFactory(new PropertyValueFactory<>("address_id"));
+            customerActiveCol.setCellValueFactory(new PropertyValueFactory<>("active"));
+            customerCreateDateCol.setCellValueFactory(new PropertyValueFactory<>("create_date"));
+            customerLastUpdateCol.setCellValueFactory(new PropertyValueFactory<>("last_update"));
+
+            List<Customer> customerList = customerDAO.readAll();
+            customerObList.addAll(customerList);
+
+            tableView.setItems(customerObList);
+            tableView.getColumns().add(customerIdCol);
+            tableView.getColumns().add(customerStoreIdCol);
+            tableView.getColumns().add(customerFirstNameCol);
+            tableView.getColumns().add(customerLastNameCol);
+            tableView.getColumns().add(customerEmailCol);
+            tableView.getColumns().add(customerAddressIdCol);
+            tableView.getColumns().add(customerActiveCol);
+            tableView.getColumns().add(customerCreateDateCol);
+            tableView.getColumns().add(customerLastUpdateCol);
+        }
+    }
 
     public void updateButtonClick(ActionEvent e) {
         System.out.println("TestButton clicked! : " + choiceBox.getValue());
