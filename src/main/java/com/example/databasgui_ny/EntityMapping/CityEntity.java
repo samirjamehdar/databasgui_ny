f0c3db2d57c390d814e78ca5dd3993c3764ddd70
Samir Jamehdar <samirjamehdar@gmail.com>
2023-04-06 00:21:11 +0200

    DAOS

--- src/main/java/com/example/databasgui_ny/EntityMapping/CityEntity.java
+++ src/main/java/com/example/databasgui_ny/EntityMapping/CityEntity.java
@@ -5,7 +5,7 @@ import java.sql.Timestamp;
 import java.util.Objects;
 
 @Entity
-@Table(name = "city", schema = "sakila", catalog = "")
+@Table(name = "city", schema = "sakila")
 public class CityEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
